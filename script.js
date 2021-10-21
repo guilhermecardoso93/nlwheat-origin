94f5e0e3d4e0cf7e98de6829f2a9ba8c02e15667
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2021-10-21 20:48:41 -0300

    aula-4

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function changeSocialMediaLinks() {
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      fotoperfil.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
